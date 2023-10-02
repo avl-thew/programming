fa576e4f246ca856e6590322c51c51ae06ecee5d
user <user@ws1013402133.pm>
2023-10-02 17:00:03 +0500

    wip lab02

---
+++ lab02/lab2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{   
+    
+    double x, y, h;
+    
+    
+    x = 0.0;
+ 
+    scanf("%lf", &h);
+    int n;
+    n = 2/h + 1;
+
+    while (n)
+   
+    {
+        if (x>=0 && x<=1)
+        
+            y = cos(x)*exp(-(x*x));
+             
+        if (x>1 && x<=2)
+        
+            y = log(x+1) - sqrt(4 - x*x);
+        
+        printf("%f %f\n", x, y);
+        x += h;
+        n = n - 1;
+        
+    }
+    
+}
+     
---
+++ lab02/lab22.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{   
+    
+    double x, y, h;
+    x = 0.0;
+    scanf("%lf", &h);
+    int n, i;
+    n = 2/h ;
+
+    for(i =0; i <= n; i++)
+    {
+        if (x>=0 && x<= 1)
+          y = cos(x)*exp(-(x*x));
+        if (x>1 && x <= 2)
+          y = log(x+1) - sqrt(4 - x*x);
+        printf("%lf %lf\n", x, y);
+        x += h;
+    
+    }
+}
+
+
