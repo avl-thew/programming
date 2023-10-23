3d8e871dbefea203cff2b5d43371c17c507ae0fc
user <user@ws1013402137.pm>
2023-10-23 17:54:35 +0500

    Wip lab04

--- lab04/00_distance.py
+++ lab04/00_distance.py
@@ -14,14 +14,14 @@ sites = {
 
 distances = {}
 # TODO здесь заполнение словаря
-for city1, coord1 in cities.items():
-    distances[city1] = {}
-for city2, coord2 in cities.items():
-    if city1 != city2:
-        distance = ((coord1[0] - coord2[0]**2 + (coord1[1] - coord2[1])**2)**0,5)
-        distances[city1][city2] = distance
 
-        print(distances)
+MoscowLondon = ((sites['Moscow'][0] - sites['London'][0]) **2 + (sites['Moscow'][1] - sites['London'][1]) **2 ) ** 0.5
+MoscowParis =  ((sites['Moscow'][0] - sites['Paris'][0]) **2 + (sites['Moscow'][1] - sites['Paris'][1]) **2 ) ** 0.5
+LondonParis = ((sites['London'][0] - sites['Paris'][0]) **2 + (sites['London'][1] - sites['Paris'][1]) **2 ) ** 0.5
+distances['Moscow_and_London'] = round(MoscowLondon, 1)
+distances['Moscow_and_Paris'] = round(MoscowParis, 1)
+distances['London_and_Paris'] = round(LondonParis, 1)
+print(distances)
 
 
 
