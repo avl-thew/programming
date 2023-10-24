899bc87a40c4b79e00afdc3b9b2be73dacdc4b70
violetta <you@example.com>
2023-10-24 19:47:01 +0500

    Wip lab03

--- lab03/lab33.c
+++ lab03/lab33.c
@@ -19,16 +19,12 @@ int main()
     int n, imax1, imax2, imax3;
     printf("n -> ");
     scanf("%d", &n);
-
+    int A[n];
+    fill(n,A);
     
 
     int arr[n];
-    for (int i = 0; i < n; i++)
-
-    {
-        scanf("%d", &arr[i]);
     
-    }
     
     int max1 = arr[0], max2 = arr[0], max3 = arr[0];
     for (int i = 1; i<n; i++)
@@ -51,18 +47,18 @@ int main()
             max3 = arr[i];
             imax3 = i;
         }
-    }
+        printf("%6d", A[i]);
+    }   
     
     int sum = max1 + max2 + max3;
     int product = max1 * max2 * max3;
-    
-    
-   
     int index = ( imax1 + imax2 + imax3 ) % n;
     
     printf("%d\n", product - sum);
 
     printf("%d\n", index);
-    
+    for (int i = 0; i < n; i++);
+        printf("%6d", A[i]);
+    printf("\n");
     return 0;
 }
