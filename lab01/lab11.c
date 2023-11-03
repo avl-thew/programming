f6aff6d4b14d6b398830efd4e15975b4c3f56acc
violetta <you@example.com>
2023-11-03 18:56:37 +0500

    lab01

--- lab01/lab11.c
+++ lab01/lab11.c
@@ -2,28 +2,43 @@
 #include <math.h>
 
 int main()
+
 {
     int a,b,c,x = -1;
+    float res;
+    
+    FILE * nfile;
+    
 
-    while((a != 0) || (c != 0) || (b != 0) || (x != 0)){
+    
+    while((a != 0) || (c != 0) || (b != 0) || (x != 0))
+    {
         printf("Enter a -> ");
         scanf("%d", &a);
+        
         printf("Enter b -> ");
         scanf("%d", &b);
+        
         printf("Enter c -> ");
         scanf("%d", &c);
         
         printf("Enter x -> ");
         scanf("%d", &x);
-    
+        
         
         if (a<b && b<c)
+        
         {
             
             if (a<=x && a%7==0)
                 printf("%d\n", a);
+
             else if (a%7 != 0 && a <=x)
-                printf("%f\n" ,(float) a/(b+c) );
+            {
+                res = (float) a/(b+c);
+                printf("%f\n" , res );
+            }
+               
             if (x<a)
                 printf("no!\n");
             
@@ -36,7 +51,11 @@ int main()
             if (c<=x && c%7==0 )
                 printf("%d\n", c);
             else if (c%7 != 0 && c <=x)
-                printf("%f\n" ,(float) c/(b+a));
+            {
+                res = (float) c/(b+a);
+                printf("%f\n" , res );
+            }
+                
             if (x<c)
                 printf("no!\n");
             
@@ -48,12 +67,27 @@ int main()
             if (b<=x && b%7==0)
                 printf("%d\n" , b);
             else if(b%7 != 0 && b <=x)
-                printf("%f\n" ,(float) b/(a+c));
+            {
+                res = (float) b/(a+c);
+                printf("%f\n" , res );
+            }
+                
             if (x<b)
                 printf( "no!\n");
             
         }
+        nfile = fopen("log.txt", "w+");
+        fprintf(nfile, "-------\n");
+        fprintf(nfile, "%d\n", a);
+        fprintf(nfile, "%d\n", b);
+        fprintf(nfile, "%d\n", c);
+        fprintf(nfile, "%d\n", x);
+        fprintf(nfile, "%f\n", res);
+        
+        fclose(nfile);
+
+   
     }
-    
+   
     return 0;
-}
+}  
