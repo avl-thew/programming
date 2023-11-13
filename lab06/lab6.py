d52a4af3a56bb913a46ce95129e09813dd8933dd
user <user@ws1013402133.pm>
2023-11-13 16:10:37 +0500

    lab06

--- lab06/lab6.py
+++ lab06/lab6.py
@@ -1,40 +1,62 @@
+#1 
+
 import itertools
 alp = "АНДРЕЙ"
 ar = itertools.product(alp, repeat=6) 
 arl = []
 for i in ar:
     arl.append(list(i))
+flag = True
 count = 0
 for e in arl:
-    f = True
-    for i in range(len(e) - 1):
-        if e.count('Й') > 1 or e[0] == 'Й' or e[-1] == 'Й' or (e[i] == 'Й' and e[i + 1] == 'Е') or (e[i + 1] == 'Й' and e[i] == 'Е'):
-            f = False
-    if f == True: count += 1
+    if e.count('Й') > 1 or e[0] == 'Й' or e[-1] == 'Й' :
+        
+        for i in range(len(e)-1):
+            if (e[i] == 'Й' and e[i + 1] == 'Е') or (e[i + 1] == 'Й' and e[i] == 'Е'):
+                flag = False
+        if flag == True: count += 1
 print(count)
 
 
 
-n = 8**2020 + 4*2017 + 26 - 1
+# k=0
+# for x1 in 'андрей':
+#     for x2 in 'андрей':
+#         for x3 in 'андрей':
+#             for x4 in 'андрей':
+#                 for x5 in 'андрей':
+#                     for x6 in 'андрей':
+#                         s=x1+x2+x3+x4+x5+x6
+#                         if s.count('й')<=1 and x1!='й' and x6!='й' and s.count('ей')==0 and s.count('йе')==0:
+#                             k=k+1   
+# print(k)
+
+
+
+#2
+
+n = 8**2020 + 4**2017 + 26 - 1
 s = bin(n)[2:]
 print(s.count('1'))
 
 
 
-
+#3 
 def f(n):
-    b = set()
     for d in range (2, int(n**0.5) + 1):
         if n%d == 0:
-            b.add(d)
-            b.add(n//d)
-            break
-    return b
+            return False
+    return True
 
 m = 0
-for n in range(245690 , 245756 + 1):
-    m +=1
-    k = f(n)
-    if len(k) == 0:
-        print(n, m)
+for n in range(245690 , 245756 + 1):  
+    m += 1
+    if f(n) == True:
+        print(m, n)
+    
        
+
+
+
+
+
