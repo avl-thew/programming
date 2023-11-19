c1d36da3a5b71816321b2937f3d181bc70821d83
violetta <you@example.com>
2023-11-19 22:22:33 +0500

    lab06

---
+++ lab06/lab66.py
@@ -0,0 +1,59 @@
+import itertools
+import doctest
+def F(alp,b):
+    """
+    Составляет комбинацию по заданному условию
+    :param alp: алфавит для комбинаций
+    :param b: количчество символов в 1 комбинации
+    :return: количество комбинаций
+
+    >>> F("АНДРЕЙ",6)
+    23625
+    """
+    ar = itertools.product(alp, repeat=b)
+    arl = []
+    for i in ar:
+        arl.append(''.join(i))
+
+    count = 0
+    for e in arl:
+        e[0] == 'Й'
+        e[-1] == 'Й'
+        if e.count('Й') > 1:
+            continue
+        if  e.startswith('Й'):
+            continue
+        if  e.endswith('Й'):
+            continue
+        flag = True
+        for i in range(len(e)-1):
+            if (e[i] == 'Й' and e[i + 1] == 'Е') or (e[i + 1] == 'Й' and e[i] == 'Е'):
+                flag = False
+                break
+        if flag == True: count += 1
+    return count
+
+print(F("АНДРЕЙ",6))
+
+if __name__ == '__main__':
+    doctest.testmod(name='F', verbose=True)
+
+
+import doctest
+def F(a):
+    """
+    Подсчет количества единиц в двоичной записи числа
+    :param a: число в десятичной записи
+    :return: количество едениц в числе
+
+    >>> F(82020 + 42017 + 26 - 1)
+    5
+    """
+    a_2 = bin(a)[2:]
+    k = a_2.count('1')
+    return k
+
+print(F(82020 + 42017 + 26 - 1))
+
+if __name__ == '__main__':
+    doctest.testmod(name='F', verbose=True)
