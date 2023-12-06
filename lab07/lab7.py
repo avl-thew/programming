2a65032cc260fc5a72662910950e724afea58f8f
violetta <you@example.com>
2023-12-06 19:56:33 +0500

    lab07

--- lab07/lab7.py
+++ lab07/lab7.py
@@ -1,21 +1,34 @@
 # >>> unpack([None, [1, ({2, 3}, {'foo': 'bar'})]])
 # [None, 1, 2, 3, 'foo', 'bar']
 
-def  packet(n):
-    n = unpack([None, [1, ({2, 3}, {'foo': 'bar'})]])
-    return n
+l = [None, [1, ({2, 3}, {'foo': 'bar', 'a': 1})]]
+
+def unp(l):
+    result = []
+    for item in l:
+        if not item:
+            result.append(None)
+        if isinstance(item, (int, str)):
+            result.append(item)
+        elif isinstance(item, (list, tuple, set)):
+            result.extend((unp(item)))
+        elif isinstance(item, dict):
+            result.extend(unp(item.items()))
+    return result
+
+print(unp(l))
 
 
-# def func(i):
-#     if i == 1:
-#         print(0.3)
-#         return 0.3
-#     elif i == 2:
-#         print(- 1.5)
-#         return - 1.5
-#     else:
-#         A = (func(i-1) * func(i-2)) * ((i-1)**2)/((i+1)**3)
-#         print(A)
-#         return A
+def func(i):
+    if i == 1:
+        
+        return 0.3
+    elif i == 2:
+        
+        return -1.5
+    else:
+        A = (func(i-1) * func(i-2)) * ((i-1)**2)/((i+1)**3)
+        
+        return A
 
-# func(1)
+print(func(5))
