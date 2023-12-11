4b2b599ceeff38a36d8f18f3c136fba3be7c7193
user <user@ws1013402133.pm>
2023-12-11 16:22:16 +0500

    lab08

--- lab08/lab8.py
+++ lab08/lab8.py
@@ -8,38 +8,45 @@ def fread(filename: str):
         return file.readline()
     return read
 get_line = fread("text.txt")
-# print(get_line())
+print(get_line())
 
 # декоратор
-def logger(func):
-    def wrapper(*args, **kwargs):
-        print(f"Вызов функции {func} с аргументами {args}, {kwargs}")
-        result = func(*args, **kwargs)
-        print(f"Функция {func} вернула результат: {result}")
-        return result
-    return wrapper
-@logger
-def mult(num1,num2):
-    return num1 * num2
-# print(mult(4,7))
+def my_decorator(*args):
+    def logger(func):
+        def wrapper(*args, **kwargs):
+            print(f"Вызов функции {func} с аргументами {args}, {kwargs}")
+            result = func(*args, **kwargs)
+            print(f"Функция {func} вернула результат: {result}")
+            return result
+        return wrapper
+    return logger
+# my_file = open(('py_log.log'))
+    @logger
+    # (open('py_log.log', 'w+'))
+    def mult(num1, num2):
+        return num1 * num2
+    print(mult(4,7))
+
+
+
 
 
 ################# замыкание через декоратор
 
-def logger(func):
-    def wrapper(*args, **kwargs):
-        print(f"Вызов функции {func} с аргументами {args}, {kwargs}")
-        result = func(*args, **kwargs)
-        print(f"Функция {func} вернула результат: {result}")
-        return result
-    return wrapper
+# def logger(func):
+#     def wrapper(*args, **kwargs):
+#         print(f"Вызов функции {func} с аргументами {args}, {kwargs}")
+#         result = func(*args, **kwargs)
+#         print(f"Функция {func} вернула результат: {result}")
+#         return result
+#     return wrapper
 
-def fread(filename: str):
-    file = open(filename, "r")
-    @logger
-    def read():
-        return file.readline()
-    return read
+# def fread(filename: str):
+#     file = open(filename, "r")
+#     @logger(('py_log.log', 'w+'))
+#     def read():
+#         return file.readline()
+#     return read
 
-get_line = fread("text.txt")
-print(get_line())
+# get_line = fread("text.txt")
+# print(get_line())
