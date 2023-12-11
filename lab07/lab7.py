2453cff2080cf2cf1d4ede8d443ac566c6172cfb
user <user@ws1013402145.pm>
2023-12-11 17:15:34 +0500

    lab07

--- lab07/lab7.py
+++ lab07/lab7.py
@@ -1,26 +1,24 @@
 # >>> unpack([None, [1, ({2, 3}, {'foo': 'bar'})]])
 # [None, 1, 2, 3, 'foo', 'bar']
 
-l = [None, [1, [], ({2, 3}, {'foo': 'bar', 'a': 1})]]
-
+# l = [None, [1, [], ({2, 3}, {'foo': 'bar', 'a': 1})]]
+l = [[]]
 def unp(l):
     result = []
     for item in l:
-        if not item:
+        if item is None:
              result.append(None)
-        if not item:
-            result.append((unp(item)))
-        # if isinstance(item, ()):
-        #     result.extend(unp())
-        elif isinstance(item, (int, str)):
+        elif isinstance(item, (int, str)):   
             result.append(item)
         elif isinstance(item, (list, tuple, set)):
-            result.extend((unp(item)))
+            if item:
+                result.extend((unp(item)))    
         elif isinstance(item, dict):
-            result.extend(unp(item.items()))
+            if item:
+                result.extend(unp(item.items()))
+        else:
+            result.append(item)
     return result
-
-print(unp(l))
 
 
 
@@ -28,9 +26,15 @@ print(unp(l))
 def func(i):
     if i == 1:
         return 0.3
-    elif i == 2:
+    if i == 2:
         return -1.5
-    else:
-        A = (func(i-1) * func(i-2)) * ((i-1)**2)/((i+1)**3)
-        return A
-print(func(5))
+    return func(i - 1) * func(i - 2) * ((i - 1) ** 2) / ((i + 1) ** 3)
+
+
+
+
+if __name__ == "__main__":
+    print(unp(l))
+    for i in range(1, 10):
+        print(func(i))
+
