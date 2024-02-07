5d38be205f69823e8c9eb3d02022d204e3836dee
violetta <you@example.com>
2024-02-07 19:05:05 +0500

    lab12

--- lab12/lab12.py
+++ lab12/lab12.py
@@ -1,11 +1,19 @@
-import packege.parallelepiped as p
-import packege.tetrahedron as t
-import packege.sphere as s 
+import geometries.parallelepiped as p
+import geometries.tetrahedron as t
+import geometries.sphere as s 
 
- parallelepiped = p.Parallelepiped(3, 4, 5, "сталь")
- tetrahedron = t.Tetrahedron(2, "дерево")
- sphere = s.Sphere(1, "дерево")
+parallelepiped = p.Parallelepiped(3, 4, 5, "сталь")
+tetrahedron = t.Tetrahedron(2, "дерево")
+sphere = s.Sphere(1, "дерево")
 
- print("Объем параллелепипеда:", parallelepiped.calculate_volume())
- print("Площадь поверхности параллелепипеда:")
- print()
+print("Объем параллелепипеда:", parallelepiped.calculate_volume())
+print("Площадь поверхности параллелепипеда:", parallelepiped.calculate_surface_area())
+print("", parallelepiped.calculate_mass())
+
+print("", tetrahedron.calculate_volume())
+print("", tetrahedron.calculate_surface_area())
+print("", tetrahedron.calculate_mass())
+
+print("", sphere.calculate_volume())
+print("", sphere.calculate_surface_area())
+print("", sphere.calculate_mass())
