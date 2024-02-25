ce612f4bca6a00e8ef3a3aa42f24d82e42d27e27
violetta <you@example.com>
2024-02-25 17:01:07 +0500

    lab01

---
+++ lab01/labn2.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a,b,c,x;
+    x = -1;
+    float res;
+
+    FILE * nfile;
+
+    while (a!=0 || b!=0 || c!=0 || x!=0)
+    {
+
+        printf("Enter a -> ");
+        scanf("%d", &a);
+        printf("Enter b -> ");
+        scanf("%d", &b);
+        printf("Enter c -> ");
+        scanf("%d", &c);
+        printf("Enter x -> ");
+        scanf("%d", &x);
+        if (a == 0 && b == 0 && c == 0 && x == 0)
+            break;
+        
+        
+        
+        if (a<b && b<c)
+        {
+            
+            if (a<=x && a%7==0)
+            {
+                res = (float)a;
+                printf("%f\n", res);
+            }
+            // printf("%d\n" , a); 
+
+            else if (a%7 != 0 && a <=x)
+            {
+                res = (float) a/(b+c);
+                printf("%f\n" , res );
+            }
+                
+            if (x<a)
+                printf("no!\n");
+            
+
+        }
+
+        if (a>b && b>c)
+        {
+            
+            if (c<=x && c%7==0 )
+            {
+                res = (float)c;
+                printf("%f\n", res);
+            }
+            // printf("%d\n" , c); 
+            else if (c%7 != 0 && c <=x)
+            {
+                res = (float) c/(b+a);
+                printf("%f\n" , res );
+            }
+            if (x<c)
+                printf("no!\n");
+            
+        }
+
+        if (b<a && a<c)
+        {
+            
+            if (b<=x && b%7==0)
+            {
+                res = (float)b; 
+                printf("%f\n" , res);
+            }
+            // printf("%d\n" , b);    
+            else if(b%7 != 0 && b <=x)
+            {
+                res = (float) b/(a+c);
+                printf("%f\n" , res );
+            }
+            if (x<b)
+                printf( "no!\n");
+            
+        }
+        nfile = fopen("log.txt", "a+");
+        fprintf(nfile, "-------\n");
+        fprintf(nfile, "%d\n", a);
+        fprintf(nfile, "%d\n", b);
+        fprintf(nfile, "%d\n", c);
+        fprintf(nfile, "%d\n", x);
+        fprintf(nfile, "%f\n", res);
+        
+        fclose(nfile);
+    }
+    return 0;
+}
