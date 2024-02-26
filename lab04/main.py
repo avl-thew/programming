ef7c9b3251683ea50ab3d9d2a46966cc49319e12
user <user@ws1013402146.pm>
2024-02-26 10:38:49 +0500

    lab04

--- lab04/main.py
+++ lab04/main.py
@@ -1,1 +1,19 @@
 from distance import distance
+from logic04 import Family 
+
+
+
+
+
+
+
+# Создаем экземпляр класса Family
+my_family = Family(['Mother', 'Father', 'I'], [160, 187, 172])
+
+# Получаем и выводим высоту члена семьи 'Father'
+father_height = my_family.get_member_height('Father')
+print('Рост отца:', father_height)
+
+# Вычисляем и выводим общую сумму всех высот в семье
+total_height = my_family.get_total_height()
+print('Общий рост семьи:', total_height)
