ab6bdd0cf17645e5f12b0a5c2f8fcff92db39289
violetta <you@example.com>
2024-04-13 19:49:26 +0500

    lab5.matt

--- lab5.matt/lab5.py
+++ lab5.matt/lab5.py
@@ -1,8 +1,8 @@
-import sympy
-from sympy.abc import x
+# import sympy
+# from sympy.abc import x
 
-f = 1/(x*((x+1)**2))
-print(sympy.integrate(f, (x, 1, 2)))
+# f = 1/(x*((x+1)**2))
+# print(sympy.integrate(f, (x, 1, 2)))
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,14 +21,14 @@ h = (b-a)/n
 xs = np.linspace(a, b, n+1)
 ys = f(xs)
 
-plt.plot(x, y)
+plt.plot(x, y, color='red')
 
 for i in range(n):
     xk = a + i*h 
     yk = abs(ys[i])
-    plt.gca().add_patch(plt.Rectangle((xk, 0), h, yk, color='blue'))
+    plt.gca().add_patch(plt.Rectangle((xk, 0), h, yk, color='green'))
     if ys[i] < 0:
-        plt.gca().add_patch(plt.Rectangle((xk, -yk), h, -yk, color='blue'))
+        plt.gca().add_patch(plt.Rectangle((xk, -yk), h, -yk, color='green'))
         
 plt.title('График функции и прямоугольники')
 plt.xlabel('x')
