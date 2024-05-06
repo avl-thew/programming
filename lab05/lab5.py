42211f2b1a412fe399c1f75c3227fe7c833db4f9
user <user@ws1013402146.pm>
2024-05-06 10:30:48 +0500

    lab05

--- lab05/lab5.py
+++ lab05/lab5.py
@@ -7,10 +7,11 @@ def f(x):
 
 def f_pr(x):
     return - math.exp(-x*x) * math.sin(x) - 2 * x * math.exp(-x*x) * math.cos(x)
+
 def yk(t, x):
-    for i in x:
-        y1.append(f(t) + f_pr(t) * (i - t))
-    return y1
+    y = f(t) + f_pr(t) * (x - t)
+    return y
+
 tochka_kasaniya = 0.2
 h = float(input())
 h = round(1/h)
@@ -18,15 +19,16 @@ x = np.linspace(0,1,h)
 y = []
 for i in x:
     y.append(f(i))
-x1 = [0.0, 1.0]
-y1 =[]
+x1 = np.array([0.0, 1.0])
+
 plt.title('График') 
 plt.xlabel('x') 
 plt.ylabel('y') 
 plt.grid()
 plt.plot(x, y)
-plt.plot(x1, yk(tochka_kasaniya, x1))
+plt.plot(x1, [yk(tochka_kasaniya, xi) for xi in x1])
 plt.plot(tochka_kasaniya, f(tochka_kasaniya), "ro")
 
 plt.show()
+
 
